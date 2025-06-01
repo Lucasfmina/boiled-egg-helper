a7c93fedb11f425442e3a5fb92e3a84c413246cf
Lucasfmina <lucasfmina@outlook.com>
2025-06-01 17:06:22 -0300

    feat: add routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/mole', function (req, res) {
+  res.render('timer', { 'time': 1 });
+});
+
+router.get('/ao-ponto', function (req, res) {
+  res.render('timer', { 'time': 2 });
+});
+
+router.get('/dura', function (req, res) {
+  res.render('timer', { 'time': 3 });
+});
+
 
 module.exports = router;
